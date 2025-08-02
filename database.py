c45f8be32861b6aacd175fadbc122e01ce7fd953
Nikita <kniki098@gmail.com>
2025-08-02 18:30:23 +0530

    Changes

--- database.py
+++ database.py
@@ -1,18 +1,45 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+
+
+# URL_DATABASE = 'postgresql://postgres:1234@LocalHost:5432/AI_Interviewer'
+
+# # # URL_DATABASE = 'postgresql://user:password@host:port/dbname'
+# # import os
+# # from dotenv import load_dotenv
+
+
+# engine = create_engine(URL_DATABASE)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+
+# database.py
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# Load environment variables
+load_dotenv()
 
-URL_DATABASE = 'postgresql://postgres:1234@LocalHost:5432/AI_Interviewer'
+# Replace your local URL with environment variable
+# URL_DATABASE = 'postgresql://postgres:@LocalHost:5432/AI_Interviewer'  
+URL_DATABASE = os.getenv("DATABASE_URL")
 
-# # URL_DATABASE = 'postgresql://user:password@host:port/dbname'
-# import os
-# from dotenv import load_dotenv
-
-
+# Create engine
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
+# Dependency to get database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
